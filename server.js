78ad91a99c05d962a2f374124e2337f89d163c13
Tabbatha <tabbatha@DOBETTERs-iMac.local>
2018-10-04 15:53:49 -0400

    more server.js updates

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
+"use strict";
+
 const express = require("express");
 const app = express();
-// respond with "Hello Class!" on the homepage
-// app.get("/students", (req, res) => {
-// res.send("Hello Class!");
-// });
+const bodyParser = require("body-parser");
+//put requires below here
+// const cartItems = require("./routes/cart-list-routes");
+// const store = require("./routes/store-routes");
 
-app.get('/', (req, res) => res.send('Hello World!'))
+//put app.use below here
+app.use(bodyParser.json());
+//these will be the routes we need to use. portal is made up.
+app.use(express.static(__dirname + "/public"));
+// app.use("/portal", cartItems);
+// app.use("/portal", store);
+
 
 //set up port, 3000 or 8080, my choice!
 const port = process.env.PORT || 5000;
